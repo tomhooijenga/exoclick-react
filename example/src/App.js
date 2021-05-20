07a4de1f8a6ddd6638d94e7196a5bb83d5b81ae1
thooijenga <thooijenga@snelstart.nl>
2021-05-20 16:15:35 +0200

    cleanup example

--- example/src/App.js
+++ example/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Banner } from "./exoclick-react";
+import { useState } from "react";
 
 function App() {
+
+  const [amount, setAmount] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => setAmount(amount + 5)}>More</button>
+        <button onClick={() => setAmount(Math.max( amount - 5, 0))}>Less</button>
+      </div>
+      {new Array(amount).fill(0).map((_, i) => {
+        return <Banner zoneId={2915820} key={i} />
+      })}
+
     </div>
   );
 }
--- example/src/exoclick-react/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { TestComponent } from './components/TestComponent/TestComponent';
-export { TestComponent };
